a0e6cb132915b69a7b6c5e80791bdc0399ee4bec
tihonsavenkov <brumwellte@gmail.com>
2023-11-04 15:33:04 +0500

    DataBase

---
+++ WebApp/WebApp/DataModels/MyDBContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApp.DataModels
+{
+	public class MyDBContext : DbContext
+	{
+		public MyDBContext(DbContextOptions<MyDBContext> options) : base(options) { }
+		public DbSet<Person> Persons { get; set; }
+
+		public static void FirstMain(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			builder.Services.AddControllersWithViews();
+
+			builder.Services.AddDbContext<MyDBContext>(options =>
+			{
+				options.UseSqlite(@"Data source=\Users\tihonsavenkov\Projects\Temp");
+			});
+		}
+	}
+}
+
